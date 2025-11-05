5c9f073e591fdfbfae2a6009fad29b909d8b6bf1
Daria Ziobrowska <dziobrowska@c16577.home>
2025-11-05 21:04:39 +0100

    install dependencies and mount a router

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser') 
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+require('./db');
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const usersRouter = require("./routes/users");
 
-
-
+app.use("/api/users", usersRouter);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
